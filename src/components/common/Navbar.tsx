b1a9a2e1b5297fe601a2122c8097ca43ce069d21
ndpniraj <ndpniraj@gmail.com>
2024-07-03 19:51:20 +0530

    basic nav bar

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,9 +1,38 @@
+import {
+  Navbar as NextUINav,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Badge,
+} from "@nextui-org/react";
 import { FC } from "react";
+import { FaBookReader } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 interface Props {}
 
 const Navbar: FC<Props> = () => {
-  return <div className=""></div>;
+  return (
+    <NextUINav>
+      <NavbarBrand>
+        <Link to="/" className="flex items-center justify-center space-x-2">
+          <FaBookReader size={24} />
+          <p className="font-bold text-inherit">Store</p>
+        </Link>
+      </NavbarBrand>
+
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Link to="/cart">
+            <Badge content="0" color="danger" shape="circle">
+              <FaCartShopping size={24} />
+            </Badge>
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </NextUINav>
+  );
 };
 
 export default Navbar;
