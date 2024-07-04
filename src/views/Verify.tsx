b81ce62e088a9570569921f26ecf03f8e07b2a44
ndpniraj <ndpniraj@gmail.com>
2024-07-04 19:07:53 +0530

    dispatching action

--- src/views/Verify.tsx
+++ src/views/Verify.tsx
@@ -1,17 +1,22 @@
 import { Spinner } from "@nextui-org/react";
 import { FC } from "react";
+import { useDispatch } from "react-redux";
 import { Navigate, useSearchParams } from "react-router-dom";
+import { updateProfile } from "../store/auth";
 
 interface Props {}
 
 const Verify: FC<Props> = () => {
   const [searchPrams] = useSearchParams();
   const profileInfoString = searchPrams.get("profile");
+  const dispatch = useDispatch();
 
   if (profileInfoString) {
     try {
       const profile = JSON.parse(profileInfoString);
       if (!profile.signedUp) return <Navigate to="/new-user" />;
+
+      dispatch(updateProfile(profile));
 
       return <Navigate to="/" />;
     } catch (error) {
