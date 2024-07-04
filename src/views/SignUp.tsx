579cdda12bdaf7de433afafb709f72ad744540fb
ndpniraj <ndpniraj@gmail.com>
2024-07-04 16:23:23 +0530

    sign up with axios

--- src/views/SignUp.tsx
+++ src/views/SignUp.tsx
@@ -1,10 +1,36 @@
 import { Button, Input } from "@nextui-org/react";
-import { FC } from "react";
+import { FC, FormEventHandler, useState } from "react";
 import Book from "../svg/Book";
+import client from "../api/client";
 
 interface Props {}
 
+const emailRegex = new RegExp(
+  "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$"
+);
+
 const SignUp: FC<Props> = () => {
+  const [email, setEmail] = useState("");
+  const [invalidForm, setInvalidForm] = useState(false);
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (evt) => {
+    evt.preventDefault();
+
+    if (!emailRegex.test(email)) return setInvalidForm(true);
+
+    setInvalidForm(false);
+
+    try {
+      const { data } = await client.post("/auth/generate-link", {
+        email,
+      });
+
+      console.log(data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex-1 flex items-center justify-center">
       <div className="flex flex-col items-center justify-center w-96 border-2 p-5 rounded-md">
@@ -14,12 +40,17 @@ const SignUp: FC<Props> = () => {
           potential.
         </h1>
 
-        <form className="w-full space-y-6 mt-6">
+        <form onSubmit={handleSubmit} className="w-full space-y-6 mt-6">
           <Input
-            type="email"
             label="Email"
             placeholder="john@email.com"
             variant="bordered"
+            isInvalid={invalidForm}
+            errorMessage="Invalid email!"
+            value={email}
+            onChange={({ target }) => {
+              setEmail(target.value);
+            }}
           />
           <Button type="submit" className="w-full">
             Send Me The Link
