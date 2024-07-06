6bc3a25e2373aaa897b8de235a9122e99a2fd14c
ndpniraj <ndpniraj@gmail.com>
2024-07-06 13:22:28 +0530

    book form

--- src/components/BookForm.tsx
+++ src/components/BookForm.tsx
@@ -1,5 +1,12 @@
-import { Button } from "@nextui-org/react";
+import {
+  Autocomplete,
+  AutocompleteItem,
+  Button,
+  DatePicker,
+  Input,
+} from "@nextui-org/react";
 import { FC } from "react";
+import { genres, languages } from "../utils/data";
 
 interface Props {
   title: string;
@@ -11,6 +18,89 @@ const BookForm: FC<Props> = ({ title, submitBtnTitle }) => {
   return (
     <form className="p-10 space-y-6">
       <h1 className="pb-6 font-semibold text-2xl w-full">{title}</h1>
+
+      <label htmlFor="file">
+        <span>Select File: </span>
+        <input
+          accept="application/epub+zip"
+          type="file"
+          name="file"
+          id="file"
+        />
+      </label>
+
+      {/* Poster Selector */}
+
+      <Input
+        type="text"
+        name="title"
+        isRequired
+        label="Book Title"
+        placeholder="Think & Grow Rich"
+      />
+
+      {/* About For Book */}
+
+      <Input
+        name="publicationName"
+        type="text"
+        label="Publication Name"
+        isRequired
+        placeholder="Penguin Book"
+      />
+
+      <DatePicker label="Publish Date" showMonthAndYearPickers isRequired />
+
+      <Autocomplete
+        label="Language"
+        placeholder="Select a Language"
+        items={languages}
+      >
+        {(item) => {
+          return (
+            <AutocompleteItem key={item.name}>{item.name}</AutocompleteItem>
+          );
+        }}
+      </Autocomplete>
+
+      <Autocomplete label="Genre" placeholder="Select a Genre" items={genres}>
+        {(item) => {
+          return (
+            <AutocompleteItem key={item.name}>{item.name}</AutocompleteItem>
+          );
+        }}
+      </Autocomplete>
+
+      <div className="bg-default-100 rounded-md py-2 px-3">
+        <p className="text-xs pl-3">Price*</p>
+
+        <div className="flex space-x-6 mt-2">
+          <Input
+            name="mrp"
+            type="number"
+            label="MRP"
+            isRequired
+            placeholder="0.00"
+            startContent={
+              <div className="pointer-events-none flex items-center">
+                <span className="text-default-400 text-small">$</span>
+              </div>
+            }
+          />
+          <Input
+            name="sale"
+            type="number"
+            label="Sale Price"
+            isRequired
+            placeholder="0.00"
+            startContent={
+              <div className="pointer-events-none flex items-center">
+                <span className="text-default-400 text-small">$</span>
+              </div>
+            }
+          />
+        </div>
+      </div>
 
       <Button className="w-full">{submitBtnTitle}</Button>
     </form>
