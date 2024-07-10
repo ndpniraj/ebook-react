5877528a7e3a8963694c21984d73e124794ebc83
ndpniraj <ndpniraj@gmail.com>
2024-07-10 15:17:49 +0530

    author form validation

--- src/components/common/AuthorForm.tsx
+++ src/components/common/AuthorForm.tsx
@@ -3,13 +3,39 @@ import useAuth from "../../hooks/useAuth";
 import RichEditor from "../rich-editor";
 import { Button, Input } from "@nextui-org/react";
 import { MdClose, MdOutlineAdd } from "react-icons/md";
+import { z } from "zod";
+import ErrorList from "./ErrorList";
 
 interface Props {
   btnTitle?: string;
 }
 
+const newAuthorSchema = z.object({
+  name: z
+    .string({
+      required_error: "Name is missing!",
+      invalid_type_error: "Invalid name!",
+    })
+    .trim()
+    .min(3, "Invalid name"),
+  about: z
+    .string({
+      required_error: "About is missing!",
+      invalid_type_error: "Invalid about!",
+    })
+    .trim()
+    .min(100, "Please write at least 100 characters about yourself!"),
+  socialLinks: z
+    .array(z.string().url("Social links can only be list of  valid URLs!"))
+    .optional(),
+});
+
 const AuthorForm: FC<Props> = ({ btnTitle }) => {
   const [socialLinks, setSocialLinks] = useState([""]);
+  const [about, setAbout] = useState("");
+  const [errors, setErrors] = useState<{
+    [key: string]: string[] | undefined;
+  }>();
   const { profile } = useAuth();
 
   const addLinkFields = () => {
@@ -23,20 +49,62 @@ const AuthorForm: FC<Props> = ({ btnTitle }) => {
     setSocialLinks(oldList);
   };
 
+  const updateSocialLinks = (index: number, value: string) => {
+    const oldList = [...socialLinks];
+    oldList[index] = value;
+    setSocialLinks(oldList);
+  };
+
+  const handleSubmit = () => {
+    const links: string[] = [];
+
+    socialLinks.forEach((link) => {
+      if (link.trim()) links.push(link);
+    });
+
+    const data = {
+      name: profile?.name,
+      about,
+      socialLinks: links,
+    };
+
+    const result = newAuthorSchema.safeParse(data);
+    if (!result.success) {
+      return setErrors(result.error.flatten().fieldErrors);
+    }
+
+    console.log(result.data);
+  };
+
   return (
     <div className="p-4 space-y-6">
       <p>
         Name: <span className="font-semibold text-lg">{profile?.name}</span>
       </p>
 
-      <RichEditor editable placeholder="Say who you are to your readers..." />
+      <RichEditor
+        onChange={setAbout}
+        editable
+        placeholder="Say who you are to your readers..."
+        isInvalid={errors?.about ? true : false}
+        errorMessage={<ErrorList errors={errors?.about} />}
+      />
 
       <div className="space-y-4">
         <p className="text-sm font-semibold">Social Links:</p>
+
+        <ErrorList errors={errors?.socialLinks} />
+
         {socialLinks.map((_, index) => {
           return (
             <div className="flex items-center space-x-4">
-              <Input placeholder="https://x.com/@something" key={index} />
+              <Input
+                onChange={({ target }) =>
+                  updateSocialLinks(index, target.value)
+                }
+                placeholder="https://x.com/@something"
+                key={index}
+              />
               {socialLinks.length > 1 && (
                 <Button
                   size="sm"
@@ -57,7 +125,7 @@ const AuthorForm: FC<Props> = ({ btnTitle }) => {
         </div>
       </div>
 
-      <Button>{btnTitle}</Button>
+      <Button onClick={handleSubmit}>{btnTitle}</Button>
     </div>
   );
 };
