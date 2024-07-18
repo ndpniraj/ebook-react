aaa12854d9713da9bba9134a60a42bc44c2b8f55
ndpniraj <ndpniraj@gmail.com>
2024-07-18 20:35:47 +0530

    new payment success page with some fix

--- src/views/PaymentSuccess.tsx
+++ src/views/PaymentSuccess.tsx
@@ -1,33 +1,31 @@
 import { FC, useEffect, useState } from "react";
-import { Navigate, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import client from "../api/client";
 import { parseError } from "../utils/helper";
 
 interface Props {}
 
 const PaymentSuccess: FC<Props> = () => {
-  const [pending, setPending] = useState(true);
-  const [searchParam] = useSearchParams();
-  const sessionId = searchParam.get("session_id");
+  const [busy, setBusy] = useState(true);
+  const [searchParams] = useSearchParams();
+  const sessionId = searchParams.get("session_id");
 
   useEffect(() => {
     if (!sessionId) return;
 
-    const fetchOrderDetails = async () => {
+    const fetchOrderDetail = async () => {
       try {
         const { data } = await client.post("/order/success", { sessionId });
         console.log(data);
       } catch (error) {
         parseError(error);
       } finally {
-        setPending(false);
+        setBusy(false);
       }
     };
 
-    fetchOrderDetails();
+    fetchOrderDetail();
   }, [sessionId]);
-
-  if (!sessionId) return <Navigate to="/not-found" />;
 
   return <div></div>;
 };
