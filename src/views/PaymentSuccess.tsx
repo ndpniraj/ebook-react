9b6c5c7f3d9093a931b19a9bad8bca6ae879a9fc
ndpniraj <ndpniraj@gmail.com>
2024-07-18 20:51:48 +0530

    payment success page ui

--- src/views/PaymentSuccess.tsx
+++ src/views/PaymentSuccess.tsx
@@ -1,12 +1,29 @@
 import { FC, useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import client from "../api/client";
-import { parseError } from "../utils/helper";
+import { formatPrice, parseError } from "../utils/helper";
+import { Divider } from "@nextui-org/react";
 
 interface Props {}
 
+interface OrderItem {
+  id: string;
+  cover?: string;
+  price: string;
+  qty: number;
+  slug: string;
+  title: string;
+  totalPrice: string;
+}
+
+interface Order {
+  orders: OrderItem[];
+  totalAmount: string;
+}
+
 const PaymentSuccess: FC<Props> = () => {
   const [busy, setBusy] = useState(true);
+  const [order, setOrder] = useState<Order>();
   const [searchParams] = useSearchParams();
   const sessionId = searchParams.get("session_id");
 
@@ -16,7 +33,7 @@ const PaymentSuccess: FC<Props> = () => {
     const fetchOrderDetail = async () => {
       try {
         const { data } = await client.post("/order/success", { sessionId });
-        console.log(data);
+        setOrder(data);
       } catch (error) {
         parseError(error);
       } finally {
@@ -27,7 +44,47 @@ const PaymentSuccess: FC<Props> = () => {
     fetchOrderDetail();
   }, [sessionId]);
 
-  return <div></div>;
+  return (
+    <div className="lg:p-0 p-5">
+      <h1 className="font-semibold text-2xl">
+        Congrats Your Order is Successful.
+      </h1>
+      <div className="p-5 flex flex-col items-center">
+        {order?.orders.map((item) => {
+          return (
+            <div key={item.id} className="w-96">
+              <div className="flex">
+                <img
+                  src={item.cover}
+                  alt={item.title}
+                  className="w-28 h-40 rounded object-cover"
+                />
+
+                <div className="p-3 flex-1">
+                  <Link
+                    className="line-clamp-1 font-bold text-lg underline"
+                    to={`/book/${item.slug}`}
+                  >
+                    {item.title}
+                  </Link>
+
+                  <p>Price: {formatPrice(Number(item.price))}</p>
+                  <p>Qty: {item.qty}</p>
+                </div>
+              </div>
+
+              <Divider className="my-3" />
+            </div>
+          );
+        })}
+
+        <div className="w-96 flex items-center justify-between">
+          <p className="font-bold">Total Amount:</p>
+          <p className="font-bold">{formatPrice(Number(order?.totalAmount))}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PaymentSuccess;
