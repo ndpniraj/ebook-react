0e887c0f3cdf7dd4199004060e81122a9a57b148
ndpniraj <ndpniraj@gmail.com>
2024-07-19 17:05:25 +0530

    orders page final ui

--- src/views/Orders.tsx
+++ src/views/Orders.tsx
@@ -1,7 +1,13 @@
 import { FC, useEffect, useState } from "react";
+import dateFormat from "dateformat";
+
 import client from "../api/client";
-import { parseError } from "../utils/helper";
+import { formatPrice, parseError } from "../utils/helper";
 import Skeletons from "../components/skeletons";
+import DividerWithTitle from "../components/common/DividerWithTitle";
+import { Link } from "react-router-dom";
+import { IoCloseOutline } from "react-icons/io5";
+import { Chip, Divider } from "@nextui-org/react";
 
 interface Props {}
 
@@ -21,7 +27,7 @@ interface Orders {
   paymentId?: string;
   totalAmount: string;
   paymentStatus?: string;
-  date: Date;
+  date: string;
   orderItem: OrderItem[];
 }
 
@@ -49,7 +55,56 @@ const Orders: FC<Props> = () => {
     <div className="p-5 lg:p-0">
       <h1 className="text-xl font-semibold mb-6">Your Orders</h1>
       {orders?.map((order) => {
-        return <div key={order.id}>Order</div>;
+        return (
+          <div key={order.id}>
+            <DividerWithTitle title={dateFormat(order.date, "mmmm d yyyy")} />
+            {order.orderItem.map((product) => {
+              return (
+                <div key={product.id}>
+                  <div className="flex p-5">
+                    <img
+                      src={product.cover}
+                      alt={product.title}
+                      className="w-24 rounded"
+                    />
+
+                    <div className="px-5">
+                      <Link
+                        to={`/book/${product.slug}`}
+                        className="text-lg font-bold underline"
+                      >
+                        {product.title}
+                      </Link>
+
+                      <div className="flex items-center space-x-0.5">
+                        <p className="font-semibold">
+                          {formatPrice(Number(product.price))}
+                        </p>
+                        <IoCloseOutline />
+                        <p>{product.qty} pcs</p>
+                      </div>
+
+                      <Chip color="danger" radius="sm" className="mt-2">
+                        {formatPrice(Number(product.totalPrice))}
+                      </Chip>
+                    </div>
+                  </div>
+
+                  <div className="flex justify-end">
+                    <Divider className="w-10/12" />
+                  </div>
+                </div>
+              );
+            })}
+
+            <div className="text-right space-y-1 py-6">
+              <p className="font-semibold text-xl">
+                Total Amount: {formatPrice(Number(order.totalAmount))}
+              </p>
+              <p>Payment Status: {order.paymentStatus?.toUpperCase()}</p>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
