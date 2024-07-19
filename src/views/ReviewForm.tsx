bd0402ce5a68b14424ff6af2338bc8eadcf4b6a7
ndpniraj <ndpniraj@gmail.com>
2024-07-19 17:29:56 +0530

    updating ratings ui

--- src/views/ReviewForm.tsx
+++ src/views/ReviewForm.tsx
@@ -1,18 +1,46 @@
-import { FC } from "react";
+import { Button } from "@nextui-org/react";
+import { FC, useState } from "react";
+import { FaRegStar, FaStar } from "react-icons/fa6";
 import { useParams } from "react-router-dom";
+import RichEditor from "../components/rich-editor";
 
 interface Props {}
 
 const ReviewForm: FC<Props> = () => {
+  const [selectedRatings, setSelectedRatings] = useState<string[]>([]);
   const { bookId } = useParams();
 
+  const updateRatingChanges = (rating: number) => {
+    const newRatings = Array<string>(rating).fill("selected");
+    setSelectedRatings(newRatings);
+  };
+
+  const ratings = Array(5).fill("");
+
   return (
-    <form>
-      {/* Rating Bar */}
+    <form className="p-5 space-y-6">
+      {ratings.map((_, index) => {
+        return (
+          <Button
+            isIconOnly
+            color="warning"
+            variant="light"
+            radius="full"
+            key={index}
+            onClick={() => updateRatingChanges(index + 1)}
+          >
+            {selectedRatings[index] === "selected" ? (
+              <FaStar size={24} />
+            ) : (
+              <FaRegStar size={24} />
+            )}
+          </Button>
+        );
+      })}
 
-      {/* Editor (Rich Editor) */}
+      <RichEditor placeholder="Write about book..." />
 
-      {/* Submit Button */}
+      <Button type="submit">Submit Review</Button>
     </form>
   );
 };
