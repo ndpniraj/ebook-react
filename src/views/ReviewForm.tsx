6e7f9c21055fff7ce817a3d378fc6f8a4c7834c1
ndpniraj <ndpniraj@gmail.com>
2024-07-19 17:51:24 +0530

    fetching old reviews

--- src/views/ReviewForm.tsx
+++ src/views/ReviewForm.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@nextui-org/react";
-import { FC, FormEventHandler, useState } from "react";
+import { FC, FormEventHandler, useEffect, useState } from "react";
 import { FaRegStar, FaStar } from "react-icons/fa6";
 import { useParams } from "react-router-dom";
 import RichEditor from "../components/rich-editor";
 import toast from "react-hot-toast";
 import client from "../api/client";
 import { parseError } from "../utils/helper";
+import { AxiosError } from "axios";
 
 interface Props {}
 
@@ -13,6 +14,7 @@ const ReviewForm: FC<Props> = () => {
   const [selectedRatings, setSelectedRatings] = useState<string[]>([]);
   const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
+  const [fetching, setFetching] = useState(true);
   const { bookId } = useParams();
 
   const updateRatingChanges = (rating: number) => {
@@ -43,6 +45,32 @@ const ReviewForm: FC<Props> = () => {
   };
 
   const ratings = Array(5).fill("");
+
+  useEffect(() => {
+    const fetchReview = async () => {
+      try {
+        const { data } = await client.get("/review/" + bookId);
+        setContent(data.content || "");
+        updateRatingChanges(data.rating);
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          if (error.response?.status === 404) return;
+          parseError(error);
+        }
+      } finally {
+        setFetching(false);
+      }
+    };
+
+    fetchReview();
+  }, [bookId]);
+
+  if (fetching)
+    return (
+      <div className="text-center p-5">
+        <p>Please wait...</p>
+      </div>
+    );
 
   return (
     <form onSubmit={handleSubmit} className="p-5 space-y-6">
