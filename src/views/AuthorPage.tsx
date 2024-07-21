8f5b01cd652eaec2408fe43a691e3a6d13f7ddf8
ndpniraj <ndpniraj@gmail.com>
2024-07-21 17:59:17 +0530

    author page UI final

--- src/views/AuthorPage.tsx
+++ src/views/AuthorPage.tsx
@@ -2,8 +2,9 @@ import { FC, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import client from "../api/client";
 import { parseError } from "../utils/helper";
-import { User } from "@nextui-org/react";
+import { Link, User } from "@nextui-org/react";
 import RichEditor from "../components/rich-editor";
+import BookList, { Book } from "../components/BookList";
 
 interface Props {}
 
@@ -12,6 +13,7 @@ interface AuthorInfo {
   name: string;
   about: string;
   socialLinks: string[];
+  books: Book[];
 }
 
 const AuthorPage: FC<Props> = () => {
@@ -46,8 +48,33 @@ const AuthorPage: FC<Props> = () => {
   return (
     <div className="p-5 lg:p-0">
       <User name={authorInfo?.name} />
+
+      <div className="py-6 pl-10">
+        <h1 className="font-semibold text-lg">Social Links:</h1>
+        <div className="flex items-center space-x-2">
+          {authorInfo?.socialLinks.map((link) => {
+            const { host } = new URL(link);
+            return (
+              <div key={link}>
+                <Link
+                  className="text-gray-800 dark:text-white font-semibold underline"
+                  href={link}
+                  target="_blank"
+                >
+                  {host}
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
       <div className="pl-10 mt-3">
         <RichEditor value={authorInfo?.about} className="regular" />
+      </div>
+
+      <div className="mt-6">
+        <BookList title="Books By The Author" data={authorInfo?.books || []} />
       </div>
     </div>
   );
