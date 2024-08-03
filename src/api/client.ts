59858032b69a99e1a4fe7fe2f3796ad1f5adadbe
ndpniraj <ndpniraj@gmail.com>
2024-08-03 16:08:55 +0530

    added search loading spinner

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8989",
+  baseURL: "https://clownfish-app-8rn5a.ondigitalocean.app",
 });
 
 client.interceptors.request.use(function (config) {
