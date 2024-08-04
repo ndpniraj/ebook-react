ac8ad0af85e63d472d67b0f2d9582ed0c791c8b4
ndpniraj <ndpniraj@gmail.com>
2024-08-04 09:47:23 +0530

    updated base url conditionaly

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+let baseURL = "https://clownfish-app-8rn5a.ondigitalocean.app";
+if (import.meta.env.MODE === "development") {
+  baseURL = "http://localhost:8989";
+}
+
 const client = axios.create({
-  baseURL: "http://localhost:8989",
+  baseURL,
 });
 
 client.interceptors.request.use(function (config) {
