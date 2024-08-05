fe78d65b798d7115e15c03543068ba6a8f1cc03d
ndpniraj <ndpniraj@gmail.com>
2024-08-05 10:54:40 +0530

    added footer

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode } from "react";
 import Navbar from "./Navbar";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 interface Props {
   children: ReactNode;
@@ -16,7 +16,20 @@ const Container: FC<Props> = ({ children }) => {
   return (
     <div className="min-h-screen max-w-5xl mx-auto flex flex-col">
       <Navbar />
-      <div className="flex-1 flex flex-col">{children}</div>
+      <div className="flex-1 flex flex-col pb-20">{children}</div>
+
+      <footer className="pb-10 px-4 text-center">
+        <p>
+          Made with love by{" "}
+          <Link
+            className="font-semibold hover:underline"
+            target="_blank"
+            to="https://fsniraj.dev"
+          >
+            Niraj Dhungana
+          </Link>
+        </p>
+      </footer>
     </div>
   );
 };
